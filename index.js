67f10fe395b3c1dd1110c90dc9e5dc826613fd11
Francis Rodrigues <dev@tosystems.net>
2017-04-15 21:51:52 -0300

    ENH: Added chat message event

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ var http = require('http').Server(app)
 var io = require('socket.io')(http)
 
 app.get('/', function (req, res) {
-    res.send('<h1>Hello world</h1>')
+    // res.send('<h1>Hello world</h1>')
+    res.sendFile(__dirname + '/index.html')
 })
 
 io.on('connection', function (socket) {
     console.log('a user connected!');
+
+    // In this case, for the sake of simplicity we'll send the message to everyone,
+    // including the sender.
+    socket.on('chat message', function (msg) {
+        console.log('message: ' + msg)
+    })
 
     socket.on('disconnect', function () {
         console.log('user disconnected!')
